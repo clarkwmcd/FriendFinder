00e6263b7db4b4a75fd4f556f41755051d0f4995
clarkwmcd <clarkwmcd@gmail.com>
2018-03-20 21:10:25 -0400

    made updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,5 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var friends = require("../data/friends.js");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var newFriend;
 
 module.exports = function(app) {
 
@@ -17,43 +7,19 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-
-//CODE BELOW IS NOT WORKING
   app.post("/api/friends", function(req, res) {
-    friends.push(req.body);
+    newFriend = req.body
+    friends.push(newFriend);
     res.json(true);
-  });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  // app.post("/api/tables", function(req, res) {
-  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  //   // It will do this by sending out the value "true" have a table
-  //   // req.body is available since we're using the body-parser middleware
-  //   if (tableData.length < 5) {
-  //     tableData.push(req.body);
-  //     res.json(true);
-  //   }
-  //   else {
-  //     waitListData.push(req.body);
-  //     res.json(false);
-  //   }
-  // });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+    // var newFriendScores = newFriend.scores
+    // for(i=0; i<friends.length; i++) {
+    //   var score = friends[i].scores
+    //   for(i=0; i<newFriendScores.length; i++) {
+    //   }
+    // }
+  })
 
   app.post("/api/clear", function() {
-    // Empty out the arrays of data
     friends = [];
-
-    console.log(friends);
   });
 };
